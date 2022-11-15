456958ad3136862a59234886172c365a85d3fdda
saulvg <saulvgproyecto@gmail.com>
2022-11-15 14:23:02 +0100

    nested for

--- jsBoot/file1/typeLoops/main.js
+++ jsBoot/file1/typeLoops/main.js
@@ -1,4 +1,4 @@
-'use strict'
+/* 'use strict'
 //While
 let a = 1
 let b = 5
@@ -17,3 +17,26 @@ for(let i = 1; i < a; i++){
     asterisk += '*'
     console.log(asterisk);
 }
+
+//Nested for (multiplication tables)
+let nTables = 10
+let nNumberTablet = 10
+
+for (let i = 1; i <= nTables; i++){
+    console.log('Table of', i);
+    for(let d = 0; d <= nNumberTablet; d++){
+        console.log(`${d} * ${i} = ${d * i}`);
+    }
+} */
+
+//Prime numbers
+let num = 10
+let printt
+for (let i = 2; i < num; i++){
+    if (num % i === 0){ 
+        printt = 'No es primo'
+    }else {
+        printt = 'Es primo'
+    }
+}
+console.log(printt);
